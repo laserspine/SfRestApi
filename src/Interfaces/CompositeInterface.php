a579ac5c7fb910374006bdb1630409abbad0c847
Nathan Alessandro <nalessandro@laserspineinstitute.com>
2018-10-19 15:39:25 -0400

    Composite Interface

--- src/Interfaces/CompositeInterface.php
+++ src/Interfaces/CompositeInterface.php
@@ -6,9 +6,9 @@ interface CompositeInterface
 {
   public function query(string $query, string $count): ?\stdClass;
 
-  public function insert(string $sobject, string $count): ?\stdClass;
+  public function insert(string $sobject, array $record, string $count): ?\stdClass;
   
-  public function update(string $sobject, string $count): ?\stdClass;
+  public function update(string $sobject, array $record, string $count): ?\stdClass;
 
   public function delete(string $sobject, string $id, string $count): ?string;
 }
